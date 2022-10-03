97ecf647cad34795af53ffd9d064a58f353fab72
huntor <chenhantuo@gmail.com>
2022-10-03 17:14:50 +0800

    Add Test Base Files

---
+++ hpdf-tests/source/test-suite.h
@@ -0,0 +1,23 @@
+#ifndef TESTSUITE_H
+#define TESTSUITE_H
+
+#include <QObject>
+#include <QtTest/QtTest>
+#include <vector>
+
+namespace cm {
+class  TestSuite : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TestSuite(const QString& _testName="");
+    virtual ~TestSuite();
+
+    QString testName;
+    static std::vector<TestSuite*>& testList();
+};
+}
+
+
+
+#endif // TESTSUITE_H
